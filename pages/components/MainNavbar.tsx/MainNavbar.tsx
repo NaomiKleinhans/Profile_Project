4d42ca1bf82fc473a5ae4cbebc0b394db78715a6
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-10-13 08:08:25 +0200

    in progress

--- pages/components/MainNavbar.tsx/MainNavbar.tsx
+++ pages/components/MainNavbar.tsx/MainNavbar.tsx
@@ -6,7 +6,7 @@ const user = {
 	name: 'Tom Cook',
 	email: 'tom@example.com',
 	imageUrl:
-		'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+		'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOj12MDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
 }
 
 const navigation = [
@@ -26,10 +26,9 @@ export default function Example() {
 			{({ open }) => (
 				<>
 					<div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-						<div className='flex h-16 justify-between'>
-							<div className='flex'>
+						<div className='flex h-28 justify-between items-center'>
+							<div className='flex items-center'>
 								<div className='-ml-2 mr-2 flex items-center md:hidden'>
-									{/* Mobile menu button */}
 									<Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
 										<span className='absolute -inset-0.5' />
 										<span className='sr-only'>Open main menu</span>
@@ -53,22 +52,23 @@ export default function Example() {
 										alt='Your Company'
 									/>
 								</div>
-								<div className='hidden md:ml-6 md:flex md:items-center md:space-x-4'>
-									{navigation.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className={`${
-												item.current
-													? 'bg-gray-900 text-white'
-													: 'text-gray-300 hover:bg-gray-700 hover:text-white'
-											} rounded-md px-3 py-2 text-sm font-medium`}
-											aria-current={item.current ? 'page' : undefined}
-										>
-											{item.name}
-										</a>
-									))}
-								</div>
+							</div>
+
+							<div className='hidden md:flex md:items-center md:space-x-6 ml-auto'>
+								{navigation.map((item) => (
+									<a
+										key={item.name}
+										href={item.href}
+										className={`${
+											item.current
+												? 'bg-gray-900 text-white'
+												: 'text-gray-300 hover:bg-gray-700 hover:text-white'
+										} rounded-md px-3 py-2 text-xl font-medium`}
+										aria-current={item.current ? 'page' : undefined}
+									>
+										{item.name}
+									</a>
+								))}
 							</div>
 						</div>
 					</div>
@@ -91,7 +91,6 @@ export default function Example() {
 								</Disclosure.Button>
 							))}
 						</div>
-						
 					</Disclosure.Panel>
 				</>
 			)}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,42 @@
 import MainNavbar from "./components/MainNavbar.tsx/MainNavbar"
 import React from 'react'
 import { PlusIcon } from "./components/SvgIcons/SvgIcons"
+import Head from "next/head"
 
-const projects = [
-	{ name: 'Project 1', link: 'https://example.com/project1' },
-	{ name: 'Project 2', link: 'https://example.com/project2' },
-	{ name: 'Project 3', link: 'https://example.com/project3' }
-	// Add more projects as needed
-]
 
 const ProfilePage = () => {
 	return (
 		<>
 			<MainNavbar></MainNavbar>
-			<div className='container mx-auto p-6'>
-				<header className='text-center mb-8'>
-					<h1 className='text-3xl font-bold'>Your Name</h1>
-					<p className='text-lg'>Web Developer</p>
-				</header>
-				<div>
-					<h2 className='text-xl font-bold mb-4'>About Me</h2>
-					<p>
-						Write a brief description about yourself here. You can include your
-						skills, interests, and experience.
+			<div className='container mx-auto'>
+				<Head>
+					<title>About Me - Naomi Kleinhans</title>
+				</Head>
+				<div className='p-4'>
+					<h1 className='text-4xl font-bold'>
+						Hello, It&apos;s Me <br /> <p className="text-6xl">Naomi Kleinhans</p> And
+						I&apos;m a Frontend Developer
+					</h1>
+					<p className='mt-4'>
+						I&apos;m Naomi Kleinhans, a passionate front-end developer based in
+						South Africa. I love creating interactive and user-friendly web
+						experiences that make a difference. My journey in web development
+						started when...
 					</p>
 				</div>
-				<div className='mt-8'>
-					<h2 className='text-xl font-bold mb-4'>Projects</h2>
-					<div className='space-y-2'>
-						{projects.map((project, index) => (
-							<a
-								key={index}
-								href={project.link}
-								className='block text-blue-600 hover:underline'
-							>
-								{project.name}
-							</a>
-						))}
-					</div>
-				</div>
-				<div className='flex-shrink-0'>
-					<button
-						type='button'
-						className='relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
-					>
-						<PlusIcon
-							className='-ml-0.5 h-5 w-5'
-							aria-hidden='true'
-						/>
-						New Job
-					</button>
-				</div>
+			</div>
+			
+			<div className='flex-shrink-0'>
+				<button
+					type='button'
+					className='relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+				>
+					<PlusIcon
+						className='-ml-0.5 h-5 w-5'
+						aria-hidden='true'
+					/>
+					New Job
+				</button>
 			</div>
 		</>
 	)
